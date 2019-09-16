b7df257dcf982c6868d21503bcaba2d91c32609e
Nathan Leung <nath.leung@gmail.com>
2019-09-16 12:07:17 -0700

    using parameters

--- demo.js
+++ demo.js
@@ -1,8 +1,10 @@
-function createGreeting(){ 
-    return "Hi, my name is Rob and I\'m 25 years old"
+function createGreeting(name, age){ 
+    return `Hi, my name is ${name} and I\'m ${age} years old`
 }
 
-const greeting1 = createGreeting();
+const name = "Rob";
+const age = "25";
+const greeting1 = createGreeting(name, age);
 
 console.log(greeting1);
 
