7d44ce7b579d8ffd2ebf7a5fd26867fcfe0eaa75
Nathan Leung <nath.leung@gmail.com>
2019-09-16 12:11:43 -0700

    year of birth

--- demo.js
+++ demo.js
@@ -1,9 +1,12 @@
 function createGreeting(name, age){ 
-    return `Hi, my name is ${name} and I\'m ${age} years old`
+    const yearOfBirth = 2019 - age;
+    return `I was born in ${yearOfBirth}`
 }
 
 const name = "Rob";
 const age = "25";
+
+
 const greeting1 = createGreeting(name, age);
 
 console.log(greeting1);
