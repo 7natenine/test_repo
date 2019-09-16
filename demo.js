3ab732c31eff985d1f21fc0f7e564944f44ea1ca
robarcand <robbyarcand@aol.com>
2019-09-16 16:57:32 -0400

    final function drills

--- demo.js
+++ demo.js
@@ -1,33 +1,59 @@
+'use strict';
 function getYearOfBirth(age){
 
-    return 2019 - age;
+  return 2019 - age;
 }
 
-function createGreeting(gName, gAge){ 
-    const yob = getYearOfBirth(gAge);
-    return `My name is ${gName} I\'m ${gAge} years old and `
+function createGreeting(name, age){
+  if (name === undefined || age === undefined) {
+    throw new Error('Invalid argument.');
+  }
+	
+  if (age < 0) {
+    throw new Error('Age cannot be negative.');
+  }
+
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number');
+  }
+
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}`;
+
+}
+
+try {
+  const greeting1 = createGreeting('Rob', 25);
+  console.log(greeting1);
+} catch(e) {
+  console.log(e.message);
+}
+
+/*  const yob = getYearOfBirth(gAge);
+  return `My name is ${gName} I'm ${gAge} years old and `
             + `I was born in ${yob}`;
 }
 
 function  yearOfBirth(age){  
-    if(age < 0){
-        throw new Error("Age can not be negative");
-    }
+  if(age < 0){
+    throw new Error('Age can not be negative');
+  }
 
-    return age;
+  return age;
 }
 
 try { 
-    const greeting1 =  createGreeting(myName, myAge);
+  const greeting1 =  createGreeting(myName, myAge);
+  console.log(greeting1);
 } catch (e) {
-    console.log(e);
+  console.log(e.message);
 }
 
 
-const myName = "Rob";
+const myName = 'Rob';
 const myAge = 25;
 
 const greeting1 = createGreeting(myName, myAge);
 
 console.log(greeting1);
-
+*/
---
+++ demo2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function createGreeting(name, age) {
+  return 'Hi, my name is Rob and I\'m 25 years old.';
+}
+
+const greeting1 = createGreeting();
+console.log(greeting1);
+createGreeting(Rob, 25);
