472e2fe259d17659eda46abdfd34bb3291e061ec
Nathan Leung <nath.leung@gmail.com>
2019-09-16 13:15:25 -0700

    changing drivers

--- demo.js
+++ demo.js
@@ -6,7 +6,21 @@ function getYearOfBirth(age){
 function createGreeting(gName, gAge){ 
     const yob = getYearOfBirth(gAge);
     return `My name is ${gName} I\'m ${gAge} years old and `
-            + `I was born in ${yob}`
+            + `I was born in ${yob}`;
+}
+
+function  yearOfBirth(age){  
+    if(age < 0){
+        throw new Error("Age can not be negative");
+    }
+
+    return age;
+}
+
+try { 
+    const greeting1 =  createGreeting(myName, myAge);
+} catch (e) {
+    console.log(e);
 }
 
 
