00748fe4eb2618b3967b865b2de74830f3149f1d
robarcand <robbyarcand@aol.com>
2019-09-16 17:03:47 -0400

    final function drills 2

--- demo2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function createGreeting(name, age) {
-  return 'Hi, my name is Rob and I\'m 25 years old.';
-}
-
-const greeting1 = createGreeting();
-console.log(greeting1);
-createGreeting(Rob, 25);
